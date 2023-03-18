716d5b84f3d2ce897b669e80a87dd38c756af0b3
micha <michel.plainchamp@gmail.com>
2023-03-18 21:15:24 +0100

    work on JwtAuthenticationFilter

--- src/main/java/com/example/jwtauthentication/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/jwtauthentication/config/JwtAuthenticationFilter.java
@@ -6,10 +6,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -60,6 +64,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // the second part checks if the user is not yet connected
         if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if(jwtService.isTokenValid(jwt, userDetails)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
         }
+        // Like next() in ExpresJS :
+        filterChain.doFilter(request, response);
     }
 }
---
+++ src/main/java/com/example/jwtauthentication/config/SecurityConfiguration.java
@@ -0,0 +1,2 @@
+package com.example.jwtauthentication.config;public class SecurityConfiguration {
+}
