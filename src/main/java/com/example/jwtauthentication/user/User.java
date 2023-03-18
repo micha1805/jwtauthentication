828daabb3b5ac745c16c63bb6808f6679dcf9c19
micha <michel.plainchamp@gmail.com>
2023-03-18 01:12:54 +0100

    added User model

--- src/main/java/com/example/jwtauthentication/user/User.java
+++ src/main/java/com/example/jwtauthentication/user/User.java
@@ -1,2 +1,29 @@
-package com.example.jwtauthentication.User;public class User {
+package com.example.jwtauthentication.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Lombok : for getters and setters for all fields and toString
+@Data
+// Desing pattern Builder
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(
+        name="users"
+)
+public class User {
+    @Id
+    // By default the following is strategy = GenerationType.AUTO
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 }
