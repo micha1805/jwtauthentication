7f810218d281637fd0050b70eca99f2f4c244c0c
micha <michel.plainchamp@gmail.com>
2023-03-18 20:51:33 +0100

    added ApplicationConfig, can fetch user now

--- src/main/java/com/example/jwtauthentication/config/ApplicationConfig.java
+++ src/main/java/com/example/jwtauthentication/config/ApplicationConfig.java
@@ -1,2 +1,23 @@
-package com.example.jwtauthentication.config;public class ApplicationConfig {
+package com.example.jwtauthentication.config;
+
+import com.example.jwtauthentication.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository repository;
+    @Bean
+    public UserDetailsService userDetailsService(){
+        // the following is a lambda expression, but by default it is the full class
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 }
