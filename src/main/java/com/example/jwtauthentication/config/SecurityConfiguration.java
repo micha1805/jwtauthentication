f4c9d84bf5378d886e8dd223f131034fc974d576
micha <michel.plainchamp@gmail.com>
2023-03-18 21:15:51 +0100

    added SecurityConfig

--- src/main/java/com/example/jwtauthentication/config/SecurityConfiguration.java
+++ src/main/java/com/example/jwtauthentication/config/SecurityConfiguration.java
@@ -1,2 +1,44 @@
-package com.example.jwtauthentication.config;public class SecurityConfiguration {
+package com.example.jwtauthentication.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    // These 2 are injected by Spring :
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                // WHITE LIST, everything forn there is accepted :
+                .requestMatchers("")
+                .permitAll()
+                // BUT any other request should be authenticated
+                .anyRequest()
+                .authenticated()
+                .and() // to add a new configuration
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Token based auth is stateless
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // must be before
+
+
+        return http.build();
+    }
 }
