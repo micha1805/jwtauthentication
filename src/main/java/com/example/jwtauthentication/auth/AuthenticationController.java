e1743139e10e41b2126c5438b68f2f9e2c274ecb
micha <michel.plainchamp@gmail.com>
2023-03-18 21:29:20 +0100

    first draft of authentication controller

--- src/main/java/com/example/jwtauthentication/auth/AuthenticationController.java
+++ src/main/java/com/example/jwtauthentication/auth/AuthenticationController.java
@@ -1,2 +1,26 @@
-package com.example.jwtauthentication.auth;public class AuthenticationController {
+package com.example.jwtauthentication.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/va/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
+        //
+    }
+
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request){
+        //
+    }
+
 }
