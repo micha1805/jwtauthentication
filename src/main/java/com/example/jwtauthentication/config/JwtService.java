190fd3018de6f5d02cfe63ee49ae45e0123703a1
micha <michel.plainchamp@gmail.com>
2023-03-18 18:16:46 +0100

    added way to extract claims for the toke, IMPORTANT : SECRET KEY IS PlAIN TEXT, must put it in env var later

--- src/main/java/com/example/jwtauthentication/config/JwtService.java
+++ src/main/java/com/example/jwtauthentication/config/JwtService.java
@@ -1,12 +1,47 @@
 package com.example.jwtauthentication.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
 
+    // TODO IMPORTANT : PUT THIS KEY IN A ENV VARIABLE !!!
+    // THIS IS JUST FOR DEMO 
+    private static final String SECRET_KEY = "67556B58703273357638792F423F4528472B4B6250655368566D597133743677"
+
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extracClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extracAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+
+
+    // extract the payload of the jwt, the claims :
+    private Claims extracAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes)
     }
 }
