d4ea0e25725e3cdbc6b1c751b30dbeeb3a45af82
micha <michel.plainchamp@gmail.com>
2023-03-18 18:34:23 +0100

    added some code in JwtService

--- src/main/java/com/example/jwtauthentication/config/JwtService.java
+++ src/main/java/com/example/jwtauthentication/config/JwtService.java
@@ -2,11 +2,17 @@ package com.example.jwtauthentication.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 @Service
@@ -19,7 +25,8 @@ public class JwtService {
 
 
     public String extractUsername(String token) {
-        return null;
+        // Subject is email or username of the user,
+        return extracClaim(token, Claims::getSubject);
     }
 
     public <T> T extracClaim(String token, Function<Claims, T> claimsResolver){
@@ -27,7 +34,36 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+    public String generateToken(
+            Map<String, Objects> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                // Valid for a day :
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*24))
+                .signWith(getSigningKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
 
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extracClaim(token, Claims::getExpiration)
+    }
 
 
     // extract the payload of the jwt, the claims :
